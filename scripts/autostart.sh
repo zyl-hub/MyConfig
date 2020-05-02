455fe5ad00f3c3245b1d34107942dce3e0dbe3d2
zyl-hub <1016076582@qq.com>
2020-05-02 18:43:26 +0800

    narsnt

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,9 +6,12 @@ fcitx &
 # feh --bg-scale /home/yunlang/WallPaper/690.jpg &
 feh --bg-scale /home/yunlang/WallPaper/748.jpg &
 picom -b &
+xrandr --output eDP-1 --off
+xrandr --output eDP-1 --auto
 echo 1 | sudo -S modprobe nvidia
 /bin/bash ~/MyConfig/scripts/tap-to-click.sh &
 /bin/bash ~/MyConfig/scripts/inverse-scroll.sh &
-/bin/bash ~/MyConfig/scripts/setxmodmap-colemak.sh &
+# /bin/bash ~/MyConfig/scripts/liveWallPaper.sh &
+# /bin/bash ~/MyConfig/scripts/setxmodmap-colemak.sh &
 nm-applet &
 xfce4-power-manager &
---
+++ scripts/liveWallPaper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xwinwrap -ni -fs -s -st -sp -b -nf -ov -- mplayer -nosound -loop 0 -wid WID /home/yunlang/Wall/15166269234e3e2d1f4096ce6d.mp4_last.mp4
