6054ca95a5b694552e6f3a8858d48966b09baaed
zyl-hub <1016076582@qq.com>
2020-05-05 23:22:04 +0800

    Add

--- dmenu/dmenu.c
+++ dmenu/dmenu.c
@@ -576,20 +576,20 @@ insert:
 		if (lines > 0)
 			return;
 		/* fallthrough */
-	case XK_Down:
+	case XK_Tab:
 		if (sel && sel->right && (sel = sel->right) == next) {
 			curr = next;
 			calcoffsets();
 		}
 		break;
-	case XK_Tab:
-		if (!sel)
-			return;
-		strncpy(text, sel->text, sizeof text - 1);
-		text[sizeof text - 1] = '\0';
-		cursor = strlen(text);
-		match();
-		break;
+	case XK_Down:
+    if (!sel)
+      return;
+    strncpy(text, sel->text, sizeof text - 1);
+    text[sizeof text - 1] = '\0';
+    cursor = strlen(text);
+    match();
+    break;
 	}
 
 draw:
