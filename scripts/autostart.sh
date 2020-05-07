d46dd13de8045e852ff398feea7ceb60087dbb84
zyl-hub <1016076582@qq.com>
2020-05-07 09:18:32 +0800

    Add new line

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -8,7 +8,7 @@ feh --bg-scale /home/yunlang/WallPaper/748.jpg &
 picom -b &
 # xrandr --output eDP-1 --off
 # xrandr --output eDP-1 --auto
-echo 1 | sudo -S modprobe nvidia
+echo qwfpars-2003 | sudo -S modprobe nvidia
 /bin/bash ~/MyConfig/scripts/tap-to-click.sh &
 /bin/bash ~/MyConfig/scripts/inverse-scroll.sh &
 # /bin/bash ~/MyConfig/scripts/liveWallPaper.sh &
