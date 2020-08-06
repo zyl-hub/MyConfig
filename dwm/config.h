d48cb0bd97ba693a9043b4f3e31f5724bce903b9
zyl-hub <7542214+zyl_hub@user.noreply.gitee.com>
2020-08-06 15:11:54 +0800

    add

--- dwm/config.h
+++ dwm/config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+static const char *emacs[] = {"emacs", NULL  };
 static const char *upvol[]   = { "/home/yunlang/MyConfig/scripts/vol-up.sh",  NULL };
 static const char *downvol[] = { "/home/yunlang/MyConfig/scripts/vol-down.sh",  NULL };
 static const char *mutevol[] = { "/home/yunlang/MyConfig/scripts/vol-toggle.sh",  NULL };
@@ -90,6 +90,7 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = emacs } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
