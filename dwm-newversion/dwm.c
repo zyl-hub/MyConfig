df7bd52c9e0ea9521a6a37aa3687e875e6eae561
zyl_hub <1016076582@qq.com>
2020-09-06 18:59:03 +0800

    Add colorbar

--- dwm-newversion/dwm.c
+++ dwm-newversion/dwm.c
@@ -78,7 +78,7 @@
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel, SchemeHid }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeHid, SchemeStatus, SchemeTagsSel, SchemeTagsNorm, SchemeInfoSel, SchemeInfoNorm }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetSystemTray, NetSystemTrayOP, NetSystemTrayOrientation, NetSystemTrayOrientationHorz,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
@@ -891,7 +891,7 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
+		drw_setscheme(drw, scheme[SchemeStatus]);
 		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
 	}
@@ -911,12 +911,12 @@ drawbar(Monitor *m)
 		continue;
 
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
+	drw_setscheme(drw, scheme[SchemeTagsNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - stw - x) > bh) {
@@ -927,11 +927,11 @@ drawbar(Monitor *m)
 				if (!ISVISIBLE(c))
 					continue;
 				if (m->sel == c)
-					scm = SchemeSel;
+					scm = SchemeInfoSel;
 				else if (HIDDEN(c))
 					scm = SchemeHid;
 				else
-					scm = SchemeNorm;
+					scm = SchemeInfoNorm;
 				drw_setscheme(drw, scheme[scm]);
 
 				if (remainder >= 0) {
@@ -944,7 +944,7 @@ drawbar(Monitor *m)
 				x += tabw;
 			}
 		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
+			drw_setscheme(drw, scheme[SchemeInfoNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/bin/bash ~/MyConfig/scripts/dwm-status.sh &
+#/bin/bash ~/MyConfig/scripts/dwm-status.sh &
+/bin/bash ~/dwm-bar/dwm_bar.sh &
 #redshift-gtk &
 fcitx &
-# feh --bg-scale /home/yl/WallPaper/690.jpg &
 feh --bg-scale /home/yl/WallPaper/AKFox.jpg &
-picom -b &
+#picom -b &
 # xrandr --output eDP-1 --off
 # xrandr --output eDP-1 --auto
 # echo qwfpars-2003 | sudo -S modprobe nvidia
@@ -14,5 +14,5 @@ picom -b &
 # /bin/bash ~/MyConfig/scripts/liveWallPaper.sh &
 # setxkbmap us colemak
 # /bin/bash ~/MyConfig/scripts/setxmodmap-colemak.sh &
-nm-applet &
+nm-applet --no-agent &
 xfce4-power-manager &
